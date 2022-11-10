d8034b5107b8ea2626ec9169cb220fd47c5f7159
kopihannan <kopihannan3@gmail.com>
2022-11-10 19:19:26 +0600

    service details section created

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../components/Main/Main";
 import Blog from "../components/Pages/Blog";
+import Details from "../components/Pages/Details";
 import Home from "../components/Pages/Home";
 import Service from "../components/Pages/Service";
 
 const router = createBrowserRouter([{
     path: '/', element: <Main></Main>,
     children: [
-        { path: '/', element: <Home></Home>},
-        {path: '/service', element: <Service></Service>},
-        {path: '/blog', element: <Blog></Blog>}
+        { path: '/', element: <Home></Home> },
+        { path: '/service', element: <Service></Service> },
+        { path: '/blog', element: <Blog></Blog> },
+        {
+            path: '/service/:id', element: <Details></Details>,
+            loader: async ({ params }) => {
+                return fetch(`http://localhost:5000/service/${params.id}`);
+            },
+        },
 
     ]
 }])
