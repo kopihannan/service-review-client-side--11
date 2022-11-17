e8137bd2f28c0793a39192281c5517d0ca82e5fe
kopihannan <kopihannan3@gmail.com>
2022-11-17 12:27:45 +0600

    review delete and not found review

--- src/components/MyReviews/MyReview.js
+++ src/components/MyReviews/MyReview.js
@@ -6,11 +6,26 @@ const MyReview = () => {
     const { user } = useContext(AuthContext);
     const [myReviews, setMyReviews] = useState([]);
 
+
+
     useEffect(() => {
         fetch(`http://localhost:5000/reviews?email=${user?.email}`)
             .then(rel => rel.json())
             .then(data => setMyReviews(data))
-    }, [])
+    }, [user?.email, myReviews])
+
+    const handleDelete = (id) => {
+        const confirm = window.confirm("Are you sure you want to delete your review?");
+        if (confirm) {
+            fetch(`http://localhost:5000/reviews/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+        }
+    }
+
+
     return (
         <div>
             <div className="overflow-x-auto w-full">
@@ -19,11 +34,9 @@ const MyReview = () => {
                     <thead>
                         <tr>
                             <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
+                                REMOVE
                             </th>
-                            <th>Name</th>
+                            <th>Image & Name</th>
                             <th>Job</th>
                             <th>Favorite Color</th>
                             <th></th>
@@ -32,13 +45,18 @@ const MyReview = () => {
                     <tbody>
 
                         {
-                            myReviews.map(myReview => <MyReviewsCard myReview={myReview} key={myReview._id}></MyReviewsCard>)
+                            myReviews.length > 0 ? myReviews.map(myReview => <MyReviewsCard myReview={myReview} key={myReview._id} handleDelete={handleDelete}></MyReviewsCard>) : <div className='text-center py-28'>
+                                <p className='font-bold text-red-500 text-4xl'>Review Not Found</p>
+                            </div>
                         }
                     </tbody>
 
                 </table>
             </div>
 
+            <div>
+
+            </div>
         </div>
     );
 };
--- src/components/MyReviews/MyReviewsCard.js
+++ src/components/MyReviews/MyReviewsCard.js
@@ -1,37 +1,41 @@
 import React from 'react';
 
-const MyReviewsCard = ({ myReview }) => {
-    console.log(myReview);
+const MyReviewsCard = ({ myReview, handleDelete }) => {
+    const { foodImg, serviceName, message, _id } = myReview;
+
+
     return (
 
-        <tr>
-            <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-            </th>
-            <td>
-                <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                        <div className="mask w-24 h-24">
-                            <img src={myReview.foodImg} alt="Avatar Tailwind CSS Component" />
+            <tr>
+                <th>
+                    <label>
+                        <button onClick={() => handleDelete(_id)} className="btn btn-square btn-outline">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button>
+                    </label>
+                </th>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="mask w-24 h-24">
+                                <img src={foodImg} alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{serviceName}</div>
                         </div>
                     </div>
+                </td>
+                <td>
                     <div>
-                        <div className="font-bold">{myReview.serviceName}</div>
+                        <p>{message}</p>
                     </div>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <p>{myReview.message}</p>
-                </div>
-            </td>
-            <td>Purple</td>
-            <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-        </tr>
+                </td>
+                <td>Purple</td>
+                <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+            </tr>
     );
 };
 
